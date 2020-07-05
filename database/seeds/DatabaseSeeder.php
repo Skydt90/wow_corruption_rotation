041b476d358d24ce85d09ba935671d5c689b2823
Skydt <Christian_skydt@hotmail.com>
2020-07-05 18:28:48 +0200

    seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Corruption;
+use App\Models\Picture;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,16 +15,27 @@ class DatabaseSeeder extends Seeder
     {
         $images  = collect(array_slice(scandir(storage_path() .'/app/public/images'), 2));
         
-        $preserved = json_decode(file_get_contents(storage_path() .'/app/public/corruptions.json'))->corruptions;
-
-
+        $preserved = collect(json_decode(file_get_contents(storage_path() .'/app/public/corruptions.json'))->corruptions);
+        
         foreach($preserved as $item) {
+
+            $name        = explode(': ', $item->name)[1];
+            $rank        = substr($item->preview_item->spells[0]->spell->name, -1);
+            $spellId     = $item->preview_item->spells[0]->spell->id;
+            $description = explode("N'Zoth's Corruption.", $item->preview_item->spells[0]->description)[1];
+            $description = trim(str_replace("\r\n\r\n\r\n", " ", $description));
+            $description = substr($description, strpos($description, "\r\n") + 2);
             
-            $corruption = Corruption::create([
-                'name'         => $item->name . " " . substr($item->preview_item->spells[0]->spell->name, -1),
-                'description'  => trim(str_replace("\r\n\r\n\r\n", " ", explode("Use: Imbue an item with a malignant strain of N'Zoth's Corruption.", $item->preview_item->spells[0]->description)[1])),
-                'wowhead_link' => "https://www.wowhead.com/spell={$item->preview_item->spells[0]->spell->id}",
+            $corruption = Corruption::firstOrCreate([
+                'name'         => "$name - $rank",
+                'description'  => $description,
+                'wowhead_link' => "https://www.wowhead.com/spell=$spellId",
                 'corr_cost'    => $item->preview_item->name_description->display_string
+            ]);
+            
+            $picture = Picture::firstOrCreate([
+                'path' => 'public/images/'.strtolower($name).".jpg",
+                'corruption_id' => $corruption->id,
             ]);
         
         }
