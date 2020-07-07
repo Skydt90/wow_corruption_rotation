2abd0e35cb26f042ab7c10c27b9022bb98fbe816
Skydt90 <christian_skydt@hotmail.comgit config --global user.email christian_skydt@hotmail.com>
2020-07-08 01:51:37 +0200

    more updates

---
+++ resources/views/schedule/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container bg-info">
+    <h2 class="text-center">Corruption Vendor</h2>
+    <div class="row bg-warning">
+        <div class="col-md-4"></div>
+        <div class="col-md-4 bg-success">
+            <div class="card">
+                <div class="card-header text-center">
+                  Rotation 1
+                </div>
+                <div class="card-body">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                    <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                </div>
+              </div>
+        </div>
+        <div class="col-md-4"></div>
+    </div>
+</div>
+
+
+
+
+
+@endsection
